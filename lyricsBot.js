4fce8f4908966759c826fff5eed709fc8ee5349f
David Yuen <david@coverhound.com>
2014-05-17 14:57:26 -0700

    No bum bums

--- lyricsBot.js
+++ lyricsBot.js
@@ -3,9 +3,9 @@ var request = require('request');
 var cheerio = require('cheerio');
 
 var config = {
-  channels: ["#"],
+  channels: ["#bestcohort"],
   server: "irc.foonetic.net",
-  botName: "LyricsBot"
+  botName: "LyricsDubs"
 };
 
 var bot = new irc.Client(config.server, config.botName, {
